51bd120773c95e94d96d24655b0ce6d785e7b89a
Allen Roberts <coryroberts5699@gmail.com>
2021-05-11 10:53:48 -0400

    Buttons now route to each of the other pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,9 @@
 import { Button, Center, Flex, Heading, Text } from "@chakra-ui/react";
 import {HStack} from "@chakra-ui/react"
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
+  const router = useRouter()
   return (
     <Flex
       w="100%"
@@ -11,14 +13,28 @@ const Navbar = () => {
     >
       <Center>
       <HStack spacing="24px">
-  <Button colorScheme="teal" variant="solid">
+  <Button 
+  colorScheme="teal" 
+  variant="solid" 
+  onClick={() => router.push('/projects')}
+  >
     Projects
   </Button>
-  <Button colorScheme="teal" variant="solid">
-    Resume
+  <Button 
+  colorScheme="teal" 
+  variant="solid" 
+  href="../resume.js" 
+  onClick={() => router.push('/resume')}
+  >
+    Resume 
   </Button>
-  <Button colorScheme="teal" variant="solid">
-    About
+  <Button 
+  colorScheme="teal" 
+  variant="solid" 
+  href="../about.js" 
+  onClick={() => router.push('/about')}
+  >
+    About 
   </Button>
   
 </HStack>
